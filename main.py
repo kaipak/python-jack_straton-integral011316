c90ce3b1f5dfc2e668669098fea68b8fef6e5cae
Kai Pak <kai@kaipak.org>
2016-02-02 22:00:38 -0800

    update commit

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from sympy.mpmath import *
 
 
 def main():
-    mp.dps = 32
+    mp.dps = 64
     pi = .6
     pf = .4
     # Boltzmann constant in Hartrees
-    kB  = 3.1668114**-6
+    kB  = 3.1668114*10**-6
+    # Use Kelvin
     temp = 1500
     g0 = 0.23544264832709902
 
-
-    # Use Kelvin
-    print quad(lambda x: ((exp(-x)*(g0**2 + 2*kB*temp*x)**3)/(4*sqrt(2)*(kB*temp)**2.5*x**1.5))\
-                          *atan((sqrt(2*kB*temp*x)/pf))*atan((sqrt(2*kB*temp*x)/pi)), [0, inf])
+    while temp < 10000:
+      print temp, quad(lambda x: ((e**-x*(g0**2 + 2*kB*temp*x)**3)/(4*sqrt(2)*(kB*temp)**2.5*x**1.5))*(atan((sqrt(2*kB*temp*x)/pf))*atan((sqrt(2*kB*temp*x)/pi))), [0, inf])
+      temp += 500
 
 
 
