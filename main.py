094b325c63ffd84af87c614137aa775daef1aeb6
Kai Pak <kai@kaipak.org>
2016-02-08 07:47:06 -0800

    Testing on Sympy implementation of generalized hypergeometric function which seems to give inconsistent results with Mathematica.

--- main.py
+++ main.py
@@ -13,13 +13,50 @@ def main():
     # Use Kelvin
     temp = 1500
     g0 = 0.23544264832709902
+    # Summation limit
+    ifny = 25
 
-    while temp < 10000:
-      print temp, quad(lambda x: ((e**-x*(g0**2 + 2*kB*temp*x)**3)/(4*sqrt(2)*(kB*temp)**2.5*x**1.5))*(atan((sqrt(2*kB*temp*x)/pf))*atan((sqrt(2*kB*temp*x)/pi))), [0, inf])
-      temp += 500
+    #print analytical(pi, pf, kB, temp, g0)
+    # print numerical(pi, pf, kB, temp, g0, ifny)
+    # print hyper_test(ifny, pi, pf)
+    #print hyper_test(2, 2.25)
+    #print my_hyper([-1,-1.5,.5,1],[-.5,-1,1.5],2.25)
+    print hyper([-1,-1.5,.5,1],[-.5,-1,1.5],2.25)
 
+def analytical(pi, pf, kB, temp, g0):
+    return quad(lambda x: ((e**-x*(g0**2 + 2*kB*temp*x)**3)/(4*sqrt(2)
+                           *(kB*temp)**2.5*x**1.5))*(atan((sqrt(2*kB*temp*x)/pf))
+                           *atan((sqrt(2*kB*temp*x)/pi))), [0, inf])
 
+def numerical(pi, pf, kB, temp, g0, ifny):
+    return nsum(lambda n: ((g0**6 + 2*kB*(.5 + n)*temp*(3*g0**4 + kB*(3 + 2*n)
+                           *temp*(kB*(5 + 2*n)*temp + 3 * g0**2)))
+                           *gamma(.5 + n)
+                           *((-2*kB*temp)/pi**2)**n*rf(.5,n)*rf(1,n)
+                           *hyper([-n, -.5-n, .5, 1], [.5-n,-n,1.5],pi**2/pf**2))
+                           /(fac(n)*rf(1.5,n)), [1,ifny])
 
+# Include some conditionals to handle weird cases that Sympy doesn't appear to
+# know about
+#
+def my_hyper(a,b,z):
+    a_zeros = 0
+    b_zeros = 0
+
+    for num in a:
+        if num == 0:
+            a_zeros += 1
+    for num in b:
+        if num == 0:
+            b_zeros += 1
+
+    if a_zeros >= b_zeros and (a_zeros != 0 and b_zeros != 0):
+        return 1
+
+    return hyper(a, b, z)
+
+def hyper_test(ifny, z):
+    return nsum(lambda n: (my_hyper([-n,-.5-n,.5,1],[.5-n,-n,1.5],z)), [0,ifny])
 
 if __name__ == '__main__':
     main()
